9b0d36b2d54c61615fbb8ec8b7ab701fd69cdc10
MoonInHo <ansdlsgh55@gmail.com>
2023-06-19 01:45:12 +0900

    사용자 인증 정보 저장방식 변경 Session -> DB

--- src/main/java/com/order/manager/controller/MemberApiController.java
+++ src/main/java/com/order/manager/controller/MemberApiController.java
@@ -17,8 +17,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
-
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
